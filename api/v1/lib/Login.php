b3745d719482e61abb764fcbc301f727b46629c3
Matheus Silva <matheus@localhost>
2016-02-24 00:00:00 -0300

    melhorias de segurança

--- api/v1/lib/Login.php
+++ api/v1/lib/Login.php
@@ -12,6 +12,35 @@ use matheus\sistemaRest\api\v1\model\Torcedor;
 class Login
 {
     /**
+    * metodo que busca o ip real do usuario
+    *
+    * @access    public
+    * @author    Matheus Silva
+    * @copyright © Copyright 2010-2016 Matheus Silva. Todos os direitos reservados.
+    * @since     14/12/2010
+    * @version   0.2
+    */
+    public static function retornaIpUsuario()
+    {
+        $http_client_ip       = $_SERVER['HTTP_CLIENT_IP'];
+        $http_x_forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        $remote_addr          = $_SERVER['REMOTE_ADDR'];
+         
+        /* verifica se o ip existe */
+        if(!empty($http_client_ip)){
+            $ip = $http_client_ip;
+            /* verifica se o acesso é de um servidor proxy */
+        } elseif(!empty($http_x_forwarded_for)){
+            $ip = $http_x_forwarded_for;
+        } else {
+            /* caso contrario pega o ip normal do usuario */
+            $ip = $remote_addr;
+        }
+
+        return $ip;
+    }//public static function retornaIpUsuario()
+
+    /**
     * metodo que faz o login do usuario no sistema
     *
     * @access    public
@@ -50,12 +79,12 @@ class Login
             $sql  .= "\n WHERE login = :torcedor";
             $sql  .= "\n AND senha   = :senha";
 
-            $salt1     = "jcxzknhxjajduhlJHDGHAQZkhyhmnk789553";
-            $salt2     = "893343hjgsjhbjlAHLKJHIDJiertokrjtkr";
-            $rand      = uniqid(rand(), true);
-            $userAgent = $_SERVER['HTTP_USER_AGENT'];
-
-            $token = hash('sha256', $userAgent.$salt2.$rand.$senha.$salt1);
+            $salt1      = "jcxzknhxjajduhlJHDGHAQZkhyhmnk789553";
+            $salt2      = "893343hjgsjhbjlAHLKJHIDJiertokrjtkr";
+            $rand       = uniqid(rand(), true);
+            $userAgent  = $_SERVER['HTTP_USER_AGENT'];
+            $ip         = self::retornaIpUsuario();
+            $token = hash('sha256', $userAgent.$salt2.$rand.$senha.$salt1.$ip);
 
             $stmt = $conexao->prepare($sql);
             $stmt->bindParam(":token", $token);
@@ -68,6 +97,7 @@ class Login
                     session_start();
                 }//if (!isset($_SESSION)) {
                 
+                $_SESSION['ip']               = $ip;
                 $_SESSION['agentUser']        = $userAgent;
                 $_SESSION['logado']           = 'ok';
                 $_SESSION['u']                = $retornoSelect['login'];
@@ -105,6 +135,7 @@ class Login
         if (!isset($_SESSION['logado'])
         || $_SESSION['logado'] != 'ok'
         || $_SESSION['agentUser'] != $_SERVER['HTTP_USER_AGENT']
+        || $_SESSION['ip'] != self::retornaIpUsuario()
         || !isset($_COOKIE['token'])) {
             self::sair($redirecionar);
 
@@ -133,9 +164,8 @@ class Login
         if (!isset($_SESSION)) {
             session_start();
         }//if (!isset($_SESSION)) {
-
+            
         setcookie('token', null, -1, '/');
-        $_SESSION['logado'] = '';
         
         $sql   = "\n UPDATE torcedor";
         $sql  .= "\n SET    token = :token";
@@ -144,14 +174,16 @@ class Login
         $token = "";
         
         require_once 'Conexao.php';
-        $conexao = Conexao::getConexao();
-        $stmt = $conexao->prepare($sql);
+        $stmt = Conexao::getConexao()->prepare($sql);
         $stmt->bindParam(":torcedor", $_SESSION['u']);
         $stmt->bindParam(":token", $token);
         $stmt->execute();
-        $conexao = null;
+        $_SESSION['logado']    = '';
+        $_SESSION['u']         = '';
+        $_SESSION['agentUser'] = '';
+        $_SESSION['ip']        = '';
         session_destroy();
-
+        
         if ($redirecionar) {
             header('location:../../site/paginas/home.html');
         }//if ($redirecionar) {
